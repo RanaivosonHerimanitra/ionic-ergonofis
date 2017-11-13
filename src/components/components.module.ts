233fc3dcb804bd0fd1a6779f712180dd9d59208b
Ranaivoson <air_manitra@yahoo.fr>
2017-11-13 10:30:30 -0500

    creation de pages avec ionic

---
+++ src/components/components.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { Menu } from './menu/menu';
+@NgModule({
+	declarations: [Menu],
+	imports: [],
+	exports: [Menu]
+})
+export class ComponentsModule {}
