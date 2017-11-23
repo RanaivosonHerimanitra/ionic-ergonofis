1c9a79ba763f2e38d5264d046e0257e3055f5e5e
Ranaivoson <air_manitra@yahoo.fr>
2017-11-23 16:33:19 -0500

    plus de processing avec opencvjs

--- src/pages/tab1/tab1.ts
+++ src/pages/tab1/tab1.ts
@@ -87,14 +87,26 @@ public presentActionSheet() {
    openCVRead(event) : void {
   
     var idAttr = event.srcElement.attributes.id;
-    let mat = cv.imread(idAttr.nodeValue);
-    console.log(mat.size());
+    let src = cv.imread(idAttr.nodeValue);
+    console.log(src.size());
     //
-    console.log(cv.GaussianBlur);
-    cv.cvtColor(mat,mat ,cv.COLOR_RGB2RGBA);
+    let dst = cv.Mat.zeros(src.cols, src.rows, cv.CV_8UC3);
+   cv.cvtColor(src, src, cv.COLOR_RGBA2GRAY, 0);
+    //cv.threshold(src, src, 120, 200, cv.THRESH_BINARY);
+    let contours = new cv.MatVector();
+    let hierarchy = new cv.Mat();
+    // You can try more different parameters
+    cv.findContours(src, contours, hierarchy, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE);
+    // draw contours with random Scalar
+    for (let i = 0; i < contours.size(); ++i) {
+        let color = new cv.Scalar(111, 246, 184) ;
+        cv.drawContours(dst, contours, i, color, 1, cv.LINE_8, hierarchy, 100);
+    }
+  
+   
     this.isHidden= false;
-    cv.imshow('lastImageProcessed', mat);
-    mat.delete();
+    cv.imshow('lastImageProcessed', dst);
+    src.delete(); dst.delete(); contours.delete(); hierarchy.delete();
   
   }
 
